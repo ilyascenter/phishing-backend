f8e0c1755b76663c754bcdc9ea17193cf11cd881
ilyas <ilyasrifai007@gmail.com>
2026-03-25 14:25:43 +0700

    feature.py

---
+++ feature.py
@@ -0,0 +1,9 @@
+def extract_features(url):
+    return [
+        len(url),
+        url.count('.'),
+        1 if 'https' in url else 0,
+        1 if '@' in url else 0,
+        1 if '-' in url else 0,
+        url.count('/'),
+    ]
